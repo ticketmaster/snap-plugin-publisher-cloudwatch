2ee93c388ee05e21e3331a986769e6d4cf7073fb
Andy Chan <andy.chan@ticketmaster.com>
2016-06-09 14:44:23 -0700

    Fixing the issue when metrics data can be any type and adding unit testings

---
+++ cloudwatch/cloudwatch_test.go
@@ -0,0 +1,79 @@
+// +build unit
+
+/*
+http://www.apache.org/licenses/LICENSE-2.0.txt
+
+
+Copyright 2015 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package opentsdb
+
+import (
+	"testing"
+
+	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/core/ctypes"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCloudWatchPlugin(t *testing.T) {
+	Convey("Meta should return metadata for the plugin", t, func() {
+		meta := Meta()
+		So(meta.Name, ShouldResemble, name)
+		So(meta.Version, ShouldResemble, version)
+		So(meta.Type, ShouldResemble, plugin.PublisherPluginType)
+	})
+
+	Convey("Create CloudWatchPublisher", t, func() {
+		op := NewCloudWatchPublisher()
+		Convey("So cloudwatch publisher should not be nil", func() {
+			So(op, ShouldNotBeNil)
+		})
+		Convey("So cloudwatch publisher should be of CloudWatchPublisher type", func() {
+			So(op, ShouldHaveSameTypeAs, &opentsdbPublisher{})
+		})
+		configPolicy, err := op.GetConfigPolicy()
+		Convey("op.GetConfigPolicy() should return a config policy", func() {
+			Convey("So config policy should not be nil", func() {
+				So(configPolicy, ShouldNotBeNil)
+			})
+			Convey("So getting config policy should not return an error", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("So config policy should be a cpolicy.ConfigPolicy", func() {
+				So(configPolicy, ShouldHaveSameTypeAs, &cpolicy.ConfigPolicy{})
+			})
+			testConfig := make(map[string]ctypes.ConfigValue)
+			testConfig["namespace"] = ctypes.ConfigValueStr{Value: "snap"}
+			testConfig["region"] = ctypes.ConfigValueStr{Value: "us-east-1"}
+			cfg, errs := configPolicy.Get([]string{""}).Process(testConfig)
+			Convey("So config policy should process testConfig and return a config", func() {
+				So(cfg, ShouldNotBeNil)
+			})
+			Convey("So testConfig processing should return no errors", func() {
+				So(errs.HasErrors(), ShouldBeFalse)
+			})
+			testConfig["region"] = ctypes.ConfigValueStr{Value: "us-east-1"}
+			cfg, errs = configPolicy.Get([]string{""}).Process(testConfig)
+			Convey("So config policy should not return a config after processing invalid testConfig", func() {
+				So(cfg, ShouldBeNil)
+			})
+			Convey("So testConfig processing should return errors", func() {
+				So(errs.HasErrors(), ShouldBeTrue)
+			})
+		})
+	})
+}

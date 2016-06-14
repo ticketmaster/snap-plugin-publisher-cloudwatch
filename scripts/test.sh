82f02885cd45a7a07dbc366fc92fc990086d434f
Andy Chan <andy.chan@ticketmaster.com>
2016-06-14 11:12:31 -0700

    Fixing copyright related matters.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,31 +1,5 @@
 #!/bin/bash -e
 
-#http://www.apache.org/licenses/LICENSE-2.0.txt
-#
-#
-#Copyright 2015 Intel Corporation
-#
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
-
-# The script does automatic checking on a Go package and its sub-packages, including:
-# 1. gofmt         (http://golang.org/cmd/gofmt/)
-# 2. goimports     (https://github.com/bradfitz/goimports)
-# 3. golint        (https://github.com/golang/lint)
-# 4. go vet        (http://golang.org/cmd/vet)
-# 5. race detector (http://blog.golang.org/race-detector)
-# 6. test coverage (http://blog.golang.org/cover)
-
-# Capture what test we should run
 TEST_SUITE=$1
 
 if [[ $TEST_SUITE == "unit" ]]; then
